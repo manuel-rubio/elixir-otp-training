5276b891f4e6bd9602a3bb8668f7125e9e9a8466
Manuel Rubio <manuel@altenwald.com>
2020-03-03 14:36:30 -0800

    more coverage for tests

--- exercises/gen_server/rpn/test/rpn_test.exs
+++ exercises/gen_server/rpn/test/rpn_test.exs
@@ -7,7 +7,6 @@ defmodule RpnTest do
     Map.put(context, :rpc_pid, pid)
   end
 
-  @tag skip: true
   test "life-cycle" do
     assert {:ok, pid} = Rpn.start_link()
     assert is_pid(pid)
@@ -19,32 +18,44 @@ defmodule RpnTest do
   describe "pushing tokens" do
     setup :start_rpc
 
-    @tag skip: true
     test "addition", %{rpc_pid: pid} do
       {:ok, 5} = Rpn.push(pid, 5)
       {:ok, 7} = Rpn.push(pid, 7)
       assert {:ok, 12} = Rpn.push(pid, :+)
     end
 
-    @tag skip: true
     test "subtraction", %{rpc_pid: pid} do
       {:ok, 7} = Rpn.push(pid, 7)
       {:ok, 5} = Rpn.push(pid, 5)
       assert {:ok, 2} = Rpn.push(pid, :-)
     end
 
-    @tag skip: true
     test "multiplication", %{rpc_pid: pid} do
       {:ok, 5} = Rpn.push(pid, 5)
       {:ok, 50} = Rpn.push(pid, 50)
       assert {:ok, 250} = Rpn.push(pid, :*)
     end
 
-    @tag skip: true
     test "division", %{rpc_pid: pid} do
       {:ok, 50} = Rpn.push(pid, 50)
       {:ok, 5} = Rpn.push(pid, 5)
       assert {:ok, 10} = Rpn.push(pid, :/)
+    end
+
+    test "centigrades to farenheit (40ºC -> 104ºF)", %{rpc_pid: pid} do
+      {:ok, 9} = Rpn.push(pid, 9)
+      {:ok, 40} = Rpn.push(pid, 40)
+      {:ok, 360} = Rpn.push(pid, :*)
+      {:ok, 5} = Rpn.push(pid, 5)
+      {:ok, 72} = Rpn.push(pid, :/)
+      {:ok, 32} = Rpn.push(pid, 32)
+      assert {:ok, 104} = Rpn.push(pid, :+)
+    end
+
+    test "broken division", %{rpc_pid: pid} do
+      {:ok, 1} = Rpn.push(pid, 1)
+      {:ok, 0} = Rpn.push(pid, 0)
+      assert {:error, :edivzero} = Rpn.push(pid, :/)
     end
   end
 end
